ab55a7ce8a68da39087a7d800591407866b4ec8d
Harshdeep <harsh88maggo@gmail.com>
2019-11-21 16:17:20 +0530

    making likes

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -6,8 +6,8 @@ const User = require('../models/user');
 
 // tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
-        clientID: '694546581284-863juqja2l39vbgi4316cgtm28b3chlr.apps.googleusercontent.com', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
-        clientSecret: '-WvXWcWCvRY0ER68z9iJGwK0', // e.g. _ASDFA%KFJWIASDFASD#FAD-
+        clientID: "313233209747-dnqmail3j800a2jvsuckqhohodhs7i63.apps.googleusercontent.com",
+        clientSecret: "0FXb5EBWa4xRfJ8jR-1HKMd2",
         callbackURL: "http://localhost:8000/users/auth/google/callback",
     },
 
